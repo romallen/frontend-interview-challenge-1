246423f8da7f3636ddf9cdb598cfeee45abe0ea0
Romaine Allen <romallen1@gmail.com>
2022-05-27 16:28:04 +0900

    Add data list

--- src/views/AddModal.js
+++ src/views/AddModal.js
@@ -1,18 +1,181 @@
 import { useEffect, useState } from "react";
 import {
     Box,
-    Button,
-    Container,
-    FormLabel,
+    List,
+    ListItem,
     FormGroup,
     Modal,
     TextField,
     Typography,
   } from "@mui/material";
+  import LoadingButton from "@mui/lab/LoadingButton";
+  import axios from "axios";
+  import { customAlphabet } from "nanoid";
+
+
+  const nanoid = customAlphabet("1234567890abcdef", 13);
+  const personDataSchema = {
+    id: nanoid(),
+    title: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    gender: "",
+    address: {
+      country: "",
+      streetName: "",
+      city: "",
+      postalCode:"N/A"
+    },
+    favoriteBooks: [],
+    birthday: "",
+    favoriteColor: "",
+    comment: "",
+  };
 export function AddModal(props) {
+    const [newPersonData, setNewPersonData] = useState({personDataSchema})
+
+    const handleAddClick = () => {
+          axios
+            .post(`http://localhost:3000/persons`, newPersonData)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+              console.log("Error: ", err);
+            });
+     
+      };
+
 
     return (
         <Modal open={props.addModalOpen}>
-            <Box >ADD Modal</Box>
+           <Box
+          component="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          sx={{ bgcolor: "#ffffff", m: "0 auto", width: "70%", py: 2 }}
+        >
+          <FormGroup
+            onChange={(e) => {
+              console.log(e.target.value);
+              setNewPersonData({
+                ...newPersonData,
+                [e.target.id]: e.target.value,
+              });
+            }}
+          >
+            <List>
+              
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>Title: </Typography>
+                {/* <Typography sx={{ mr: 2 }}>{newPersonData.title} </Typography> */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}> First Name: </Typography>
+                {/* <TextField
+                  id="firstName"
+                  variant="outlined"
+                  defaultValue={newPersonData.firstName}
+                  required
+                ></TextField> */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}> Last Name: </Typography>
+                {/* <TextField
+                  id="lastName"
+                  variant="outlined"
+                  defaultValue={newPersonData.lastName}
+                  required
+                ></TextField> */}
+              </ListItem>
+         
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>Email: </Typography>
+                {/* <Typography sx={{ mr: 2 }}>{newPersonData.email} </Typography> */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>Gender: </Typography>
+                {/* <Typography sx={{ mr: 2 }}>{newPersonData.gender} </Typography> */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>Address: </Typography>
+              </ListItem>
+
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>Country: </Typography>
+                {/* <Typography sx={{ mr: 2 }}>
+                  {newPersonData.address.country}{" "}
+                </Typography> */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}> Street Name: </Typography>
+                {/* <Typography sx={{ mr: 2 }}>
+                  {newPersonData.address.streetName}
+                </Typography> */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>City: </Typography>
+                {/* <Typography sx={{ mr: 2 }}>
+                  {newPersonData.address.city}{" "}
+                </Typography> */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>Favorite Books: </Typography>
+                {/* <Typography sx={{ mr: 2 }}>
+                  {newPersonData.favoriteBooks.join(", ")}
+                </Typography> */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>Birthday: </Typography>
+
+                {/* <TextField
+                  id="birthday"
+                  defaultValue={newPersonData.birthday}
+                  type="date"
+                  variant="outlined"
+                  sx={{ mr: 1 }}
+                  required
+                ></TextField>
+                {`(${
+                  new Date().getFullYear() -
+                  new Date(newPersonData.birthday).getFullYear()
+                } years old)`} */}
+              </ListItem>
+              <ListItem>
+                <Typography sx={{ mr: 2 }}>Comment: </Typography>
+                {/* <TextField
+                  id="comment"
+                  defaultValue={newPersonData.comment}
+                  rows={2}
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                ></TextField> */}
+              </ListItem>
+            </List>
+          </FormGroup>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              mx: 2,
+            }}
+          >
+  
+              <LoadingButton
+                type="submit"
+                onClick={handleAddClick}
+                // loading={isUpdateLoading}
+                loadingIndicator="ADDING..."
+                variant="contained"
+              >
+                ADD
+              </LoadingButton>
+         
+          </Box>
+        </Box>
         </Modal>
     )}
--- src/views/DetailsModal.js
+++ src/views/DetailsModal.js
@@ -18,7 +18,10 @@ export function DetailsModal(props) {
   const [personData, setPersonData] = useState(null);
   const [isUpdateLoading, setIsUpdateLoading] = useState(false);
   const [isDeleteLoading, setIsDeleteLoading] = useState(false);
-  const [detailsError, setDetailsError] = useState({isError: false, errorMessage: null});
+  const [detailsError, setDetailsError] = useState({
+    isError: false,
+    errorMessage: null,
+  });
 
   useEffect(() => {
     if (props.id) {
@@ -26,7 +29,10 @@ export function DetailsModal(props) {
         .get(`http://localhost:3000/persons/${props.id}`)
         .then((res) => {
           if (res.data.errors) {
-            setDetailsError({isError: true, errorMessage: res.data.errors[0]});
+            setDetailsError({
+              isError: true,
+              errorMessage: res.data.errors[0],
+            });
           } else {
             setPersonData(res.data);
             props.setIsLoading(false);
@@ -34,7 +40,7 @@ export function DetailsModal(props) {
         })
         .catch((err) => {
           props.setIsLoading(false);
-          setDetailsError({isError: true, errorMessage: err.message});
+          setDetailsError({ isError: true, errorMessage: err.message });
           console.log("Error: ", err);
         });
     }
@@ -47,7 +53,7 @@ export function DetailsModal(props) {
       .patch(`http://localhost:3000/persons/${personData.id}`, personData)
       .then((res) => {
         if (res.data.errors) {
-          setDetailsError({isError: true, errorMessage: res.data.errors[0]});
+          setDetailsError({ isError: true, errorMessage: res.data.errors[0] });
         } else {
           props.setDetailsModalOpen(false);
           setIsUpdateLoading(false);
@@ -55,7 +61,7 @@ export function DetailsModal(props) {
       })
       .catch((err) => {
         setIsUpdateLoading(false);
-        setDetailsError({isError: true, errorMessage: err.message});
+        setDetailsError({ isError: true, errorMessage: err.message });
         console.log("Error: ", err);
       });
   };
@@ -66,7 +72,7 @@ export function DetailsModal(props) {
       .delete(`http://localhost:3000/persons/${personData.id}`)
       .then((res) => {
         if (res.data.errors) {
-          setDetailsError({isError: true, errorMessage: res.data.errors[0]});
+          setDetailsError({ isError: true, errorMessage: res.data.errors[0] });
         } else {
           setIsDeleteLoading(false);
           props.setDetailsModalOpen(false);
@@ -74,166 +80,172 @@ export function DetailsModal(props) {
       })
       .catch((err) => {
         setIsDeleteLoading(false);
-        setDetailsError({isError: true, errorMessage: err.message});
+        setDetailsError({ isError: true, errorMessage: err.message });
         console.log("Error: ", err);
       });
   };
 
   return (
     <Modal open={props.detailsModalOpen}>
-        <Box>
-      {detailsError.isError ? <ErrorComponent error={detailsError} setError={setDetailsError} /> : null}
-      {personData ? (
-        <Box
-          component="form"
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-          sx={{ bgcolor: personData.favoriteColor, m: "0 auto", width: "70%", py: 2 }}
-        >
-          <FormGroup
-            onChange={(e) => {
-              console.log(e.target.value);
-              setPersonData({
-                ...personData,
-                [e.target.id]: e.target.value,
-              });
-            }}
-          >
-            <List>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>ID: </Typography>
-                <Typography sx={{ mr: 2 }}>{personData.id} </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>Title: </Typography>
-                <Typography sx={{ mr: 2 }}>{personData.title} </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}> First Name: </Typography>
-                <TextField
-                  id="firstName"
-                  variant="outlined"
-                  defaultValue={personData.firstName}
-                  required
-                ></TextField>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}> Last Name: </Typography>
-                <TextField
-                  id="lastName"
-                  variant="outlined"
-                  defaultValue={personData.lastName}
-                  required
-                ></TextField>
-              </ListItem>
-              <ListItem></ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>Email: </Typography>
-                <Typography sx={{ mr: 2 }}>{personData.email} </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>Gender: </Typography>
-                <Typography sx={{ mr: 2 }}>{personData.gender} </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>Address: </Typography>
-              </ListItem>
-
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>Country: </Typography>
-                <Typography sx={{ mr: 2 }}>
-                  {personData.address.country}{" "}
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}> Street Name: </Typography>
-                <Typography sx={{ mr: 2 }}>
-                  {personData.address.streetName}
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>City: </Typography>
-                <Typography sx={{ mr: 2 }}>
-                  {personData.address.city}{" "}
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>Favorite Books: </Typography>
-                <Typography sx={{ mr: 2 }}>
-                  {personData.favoriteBooks.join(", ")}
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>Birthday: </Typography>
-
-                <TextField
-                  id="birthday"
-                  defaultValue={personData.birthday}
-                  type="date"
-                  variant="outlined"
-                  sx={{ mr: 1 }}
-                  required
-                ></TextField>
-                {`(${
-                  new Date().getFullYear() -
-                  new Date(personData.birthday).getFullYear()
-                } years old)`}
-              </ListItem>
-              <ListItem>
-                <Typography sx={{ mr: 2 }}>Comment: </Typography>
-                <TextField
-                  id="comment"
-                  defaultValue={personData.comment}
-                  rows={2}
-                  variant="outlined"
-                  sx={{ width: "100%" }}
-                ></TextField>
-              </ListItem>
-            </List>
-          </FormGroup>
-
+      <Box>
+        {detailsError.isError ? (
+          <ErrorComponent error={detailsError} setError={setDetailsError} />
+        ) : null}
+        {personData ? (
           <Box
+            component="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
             sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              mx: 2,
+              bgcolor: personData.favoriteColor,
+              m: "0 auto",
+              width: "70%",
+              py: 2,
             }}
           >
-            <LoadingButton
-              size="small"
-              startIcon={<DeleteIcon />}
-              onClick={handleDeleteClick}
-              loading={isDeleteLoading}
-              loadingIndicator="deleting..."
-              variant="contained"
+            <FormGroup
+              onChange={(e) => {
+                console.log(e.target.value);
+                setPersonData({
+                  ...personData,
+                  [e.target.id]: e.target.value,
+                });
+              }}
             >
-              DELETE
-            </LoadingButton>
-            <Box>
-              <Button
-                variant="contained"
-                sx={{ mr: 1 }}
-                onClick={() => props.setDetailsModalOpen(false)}
-              >
-                Cancel
-              </Button>
+              <List>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>ID: </Typography>
+                  <Typography sx={{ mr: 2 }}>{personData.id} </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>Title: </Typography>
+                  <Typography sx={{ mr: 2 }}>{personData.title} </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}> First Name: </Typography>
+                  <TextField
+                    id="firstName"
+                    variant="outlined"
+                    defaultValue={personData.firstName}
+                    required
+                  ></TextField>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}> Last Name: </Typography>
+                  <TextField
+                    id="lastName"
+                    variant="outlined"
+                    defaultValue={personData.lastName}
+                    required
+                  ></TextField>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>Email: </Typography>
+                  <Typography sx={{ mr: 2 }}>{personData.email} </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>Gender: </Typography>
+                  <Typography sx={{ mr: 2 }}>{personData.gender} </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>Address: </Typography>
+                </ListItem>
+
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>Country: </Typography>
+                  <Typography sx={{ mr: 2 }}>
+                    {personData.address.country}{" "}
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}> Street Name: </Typography>
+                  <Typography sx={{ mr: 2 }}>
+                    {personData.address.streetName}
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>City: </Typography>
+                  <Typography sx={{ mr: 2 }}>
+                    {personData.address.city}{" "}
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>Favorite Books: </Typography>
+                  <Typography sx={{ mr: 2 }}>
+                    {personData.favoriteBooks.join(", ")}
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>Birthday: </Typography>
+
+                  <TextField
+                    id="birthday"
+                    defaultValue={personData.birthday}
+                    type="date"
+                    variant="outlined"
+                    sx={{ mr: 1 }}
+                    required
+                  ></TextField>
+                  {`(${
+                    new Date().getFullYear() -
+                    new Date(personData.birthday).getFullYear()
+                  } years old)`}
+                </ListItem>
+                <ListItem>
+                  <Typography sx={{ mr: 2 }}>Comment: </Typography>
+                  <TextField
+                    id="comment"
+                    defaultValue={personData.comment}
+                    rows={2}
+                    variant="outlined"
+                    sx={{ width: "100%" }}
+                  ></TextField>
+                </ListItem>
+              </List>
+            </FormGroup>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                mx: 2,
+              }}
+            >
               <LoadingButton
-                type="submit"
-                onClick={handleUpdateClick}
-                loading={isUpdateLoading}
-                loadingIndicator="UPDATING..."
+                size="small"
+                startIcon={<DeleteIcon />}
+                onClick={handleDeleteClick}
+                loading={isDeleteLoading}
+                loadingIndicator="deleting..."
                 variant="contained"
               >
-                UPDATE
+                DELETE
               </LoadingButton>
+              <Box>
+                <Button
+                  variant="contained"
+                  sx={{ mr: 1 }}
+                  onClick={() => props.setDetailsModalOpen(false)}
+                >
+                  Cancel
+                </Button>
+                <LoadingButton
+                  type="submit"
+                  onClick={handleUpdateClick}
+                  loading={isUpdateLoading}
+                  loadingIndicator="UPDATING..."
+                  variant="contained"
+                >
+                  UPDATE
+                </LoadingButton>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      ) : (
-        <Box>loading</Box>
-      )}
+        ) : (
+          <Box>loading</Box>
+        )}
       </Box>
     </Modal>
   );
